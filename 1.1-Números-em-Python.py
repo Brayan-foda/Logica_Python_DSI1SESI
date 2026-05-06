809b0f1a604b09a7600d58079357356d2070aad6
Brayan <brayanpontual73@gmail.com>
2026-05-06 11:24:48 -0300

    meu primeiro programa python

---
+++ 1.1-Números-em-Python.py
@@ -0,0 +1,32 @@
+#AULA COMPLETA: NUMEROS EM PYTHON
+
+"""  ""
+1 - Tipos númericos
+2 - conversão de tipos
+3 - Hierarquia
+4 - Operação matemática
+5 - coerção de tipos
+6 - vericação de tipos
+7 - entrada de dados
+"""  ""
+############################
+# PASSO 01 - TIPOS NÚMERICOS
+############################
+#int -> números inteiros
+#float -> números com casas decimais
+#complex -> números complexos (usado em matemática/ engenharia)
+
+print("======= TIPOS NÚMERICOS ======")
+
+#EXEMPLO 01 - NUMERO INTEIRO
+
+#ciramos uma variável chamada "numero_inteiro"
+numero_inteiro = 10
+
+#mostramos o valor
+print ("Valor:", numero_inteiro)
+
+#Type() mostra qual é o tipo da variável
+print("Tipo:",type(numero_inteiro) )
+
+print("--------------------------------------")
